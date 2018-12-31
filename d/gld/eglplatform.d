a48718887218d17a30bda2b9bd4fa1226d72b67d
Remi Thebault <remi.thebault@gmail.com>
2018-12-31 11:10:33 +0100

    generated files

---
+++ d/gld/eglplatform.d
@@ -0,0 +1,16 @@
+/// Defintions from `eglplatform.h`
+module gld.eglplatform;
+
+alias EGLNativeDisplayType = void*;
+alias EGLNativePixmapType = void*;
+alias EGLNativeWindowType = void*;
+
+alias NativeDisplayType = EGLNativeDisplayType;
+alias NativePixmapType = EGLNativePixmapType;
+alias NativeWindowType = EGLNativeWindowType;
+
+alias EGLint = int;
+
+auto EGL_CAST(T, V)(V value) {
+    return cast(T)value;
+}
